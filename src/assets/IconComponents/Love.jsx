3966bcf485c0fc7a8b91bae983d7669a2c3015df
Asian Programmer <parveznew2022@gmail.com>
2025-10-08 17:27:34 +0600

    Svg error fixed

--- src/assets/IconComponents/Love.jsx
+++ src/assets/IconComponents/Love.jsx
@@ -9,8 +9,8 @@ export default function LockIcon(props) {
       viewBox="0 0 24 24"
       fill="currentColor"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
+      strokeWidth="2"
+      strokeLinecap="round"
       stroke-linejoin="round"
       class="lucide lucide-heart h-5 w-5"
     >
